4715acbd726b178891711540e05b7c446d6d9921
quan-p <m.phamquan@gmail.com>
2022-08-30 18:38:27 -0500

    Merge arrays stepping through smallest values

--- merge-sort/mergesort.js
+++ merge-sort/mergesort.js
@@ -7,5 +7,16 @@ function mergeSort(arr) {
 }
 
 function merge(leftArr, rightArr) {
-    
+    let sortedArr = [];
+    let arrWithMin;
+    while(leftArr.length > 0 && rightArr.length > 0) {
+        if(leftArr[0] < rightArr[0]) {
+                arrWithMin = leftArr;
+        } else arrWithMin = rightArr;
+        let smallerVal = arrWithMin.shift();
+        sortedArr.push(smallerVal);
+    }
+    return sortedArr.concat(leftArr, rightArr);
 }
+
+console.log(mergeSort([8, 7, 6, 5, 4, 3, 2, 1]));
